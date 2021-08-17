c1a23e2be49375f1d00b0a1e684ffe51a64e21c4
Victor <bulkin_viktor@mail.ru>
2021-08-17 17:55:14 +0300

    used the DEAP package

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from deap import base, algorithms
+from deap import creator
+from deap import tools
+
 import random
 import matplotlib.pyplot as plt
+import numpy as np
 
 # константы задачи
 ONE_MAX_LENGTH = 100    # длина подлежащей оптимизации битовой строки
@@ -13,96 +18,37 @@ MAX_GENERATIONS = 50    # максимальное количество поко
 RANDOM_SEED = 42
 random.seed(RANDOM_SEED)
 
-
-class FitnessMax():
-    def __init__(self):
-        self.values = [0]
-
-
-class Individual(list):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.fitness = FitnessMax()
-
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
 
 def oneMaxFitness(individual):
     return sum(individual), # кортеж
 
-def individualCreator():
-    return Individual([random.randint(0, 1) for i in range(ONE_MAX_LENGTH)])
+toolbox = base.Toolbox()
 
-def populationCreator(n = 0):
-    return list([individualCreator() for i in range(n)])
+toolbox.register("zeroOrOne", random.randint, 0, 1)
+toolbox.register("individualCreator", tools.initRepeat, creator.Individual, toolbox.zeroOrOne, ONE_MAX_LENGTH)
+toolbox.register("populationCreator", tools.initRepeat, list, toolbox.individualCreator)
 
+population = toolbox.populationCreator(n=POPULATION_SIZE)
 
-population = populationCreator(n=POPULATION_SIZE)
-generationCounter = 0
+toolbox.register("evaluate", oneMaxFitness)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mate", tools.cxOnePoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=1.0/ONE_MAX_LENGTH)
 
-fitnessValues = list(map(oneMaxFitness, population))
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("max", np.max)
+stats.register("avg", np.mean)
 
-for individual, fitnessValue in zip(population, fitnessValues):
-    individual.fitness.values = fitnessValue
+population, logbook = algorithms.eaSimple(population, toolbox,
+                                        cxpb=P_CROSSOVER,
+                                        mutpb=P_MUTATION,
+                                        ngen=MAX_GENERATIONS,
+                                        stats=stats,
+                                        verbose=True)
 
-maxFitnessValues = []
-meanFitnessValues = []
-
-def clone(value):
-    ind = Individual(value[:])
-    ind.fitness.values[0] = value.fitness.values[0]
-    return ind
-
-def selTournament(population, p_len):
-    offspring = []
-    for n in range(p_len):
-        i1 = i2 = i3 = 0
-        while i1 == i2 or i1 == i3 or i2 == i3:
-            i1, i2, i3 = random.randint(0, p_len-1), random.randint(0, p_len-1), random.randint(0, p_len-1)
-
-        offspring.append(max([population[i1], population[i2], population[i3]], key=lambda ind: ind.fitness.values[0]))
-
-    return offspring
-
-def cxOnePoint(child1, child2):
-    s = random.randint(2, len(child1)-3)
-    child1[s:], child2[s:] = child2[s:], child1[s:]
-
-def mutFlipBit(mutant, indpb=0.01):
-    for indx in range(len(mutant)):
-        if random.random() < indpb:
-            mutant[indx] = 0 if mutant[indx] == 1 else 1
-
-
-fitnessValues = [individual.fitness.values[0] for individual in population]
-
-while max(fitnessValues) < ONE_MAX_LENGTH and generationCounter < MAX_GENERATIONS:
-    generationCounter += 1
-    offspring = selTournament(population, len(population))
-    offspring = list(map(clone, offspring))
-
-    for child1, child2 in zip(offspring[::2], offspring[1::2]):
-        if random.random() < P_CROSSOVER:
-            cxOnePoint(child1, child2)
-
-    for mutant in offspring:
-        if random.random() < P_MUTATION:
-            mutFlipBit(mutant, indpb=1.0/ONE_MAX_LENGTH)
-
-    freshFitnessValues = list(map(oneMaxFitness, offspring))
-    for individual, fitnessValue in zip(offspring, freshFitnessValues):
-        individual.fitness.values = fitnessValue
-
-    population[:] = offspring
-
-    fitnessValues = [ind.fitness.values[0] for ind in population]
-
-    maxFitness = max(fitnessValues)
-    meanFitness = sum(fitnessValues) / len(population)
-    maxFitnessValues.append(maxFitness)
-    meanFitnessValues.append(meanFitness)
-    print(f"Поколение {generationCounter}: Макс приспособ. = {maxFitness}, Средняя приспособ.= {meanFitness}")
-
-    best_index = fitnessValues.index(max(fitnessValues))
-    print("Лучший индивидуум = ", *population[best_index], "\n")
+maxFitnessValues, meanFitnessValues = logbook.select("max", "avg")
 
 plt.plot(maxFitnessValues, color='red')
 plt.plot(meanFitnessValues, color='green')
